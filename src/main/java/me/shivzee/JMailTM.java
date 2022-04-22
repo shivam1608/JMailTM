6927bfd09daeca2c510fb5a80394c0a9bbf8206a
shivam1608 <64302819+shivam1608@users.noreply.github.com>
2022-04-22 14:44:46 +0530

    Update JMailTM.java

--- src/main/java/me/shivzee/JMailTM.java
+++ src/main/java/me/shivzee/JMailTM.java
@@ -1,7 +1,7 @@
 /*
  * The Following is the JavaWrapper Class for Mail.tm API.
  * OpenSource Library (Make PullRequest if you got something good)
- * All Rights Reserved
+ * MIT Licence
  * Author Shivzee
  * Github : https://github.com/shivam1608
  * Discord : Shivam#8010
